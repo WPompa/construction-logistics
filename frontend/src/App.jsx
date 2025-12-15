e09cf72afc1a9903221dd019a833ab3da1e33d0a
WPompa <77293165+WPompa@users.noreply.github.com>
2025-12-15 14:11:50 -0500

    Add vercel analytics package.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState, createContext } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Error from "./pages/Error";
 import Home from "./pages/Home";
 import SharedLayout from "./pages/SharedLayout";
 import ProtectedRoute from "./pages/ProtectedRoute";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export const UserContext = createContext();
 
@@ -35,6 +36,7 @@ function App() {
           <p>API Connected CRUD Web App</p>
         </footer>
       </BrowserRouter>
+      <Analytics />
     </UserContext.Provider>
   );
 }
