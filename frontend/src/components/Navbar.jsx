df6ae9b2107c801b01ddaf72490b48461e040e34
WPompa <77293165+WPompa@users.noreply.github.com>
2025-12-17 15:18:49 -0500

    Change router Links to NavLinks & set .active rule.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,28 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import CurrentUser from "./minor-components/CurrentUser";
 
 export const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navlinks">
-        <Link to="/">Home</Link>
-        <Link to="/Login">Login</Link>
-        <Link to="/Dashboard">Dashboard</Link>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/Login"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Login
+        </NavLink>
+        <NavLink
+          to="/Dashboard"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Dashboard
+        </NavLink>
       </div>
 
       <CurrentUser />
