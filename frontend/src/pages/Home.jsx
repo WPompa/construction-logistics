6f5d3f2d2622bcc82c8dd3af2833a58185399f20
WPompa <77293165+WPompa@users.noreply.github.com>
2025-12-17 15:17:35 -0500

    Remove unused code.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import { useState, useReducer } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
-import safetyHelmet from "../assets/SVG/helmet-safety.svg";
 import cogs from "../assets/SVG/cogs.svg";
 import {
   login,
