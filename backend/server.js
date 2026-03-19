3a113aec117f74134cb08cacf6abbac1f521cce8
WPompa <77293165+WPompa@users.noreply.github.com>
2026-03-19 18:39:40 -0400

    Add NPM Packages Helmet and Express-Rate-Limit.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 "use strict";
 const express = require("express");
+const app = express();
 const cors = require("cors");
 const dbConnection = require("./middleware/dbConnection");
 const auth = require("./middleware/auth");
 const { connectToDB, sequelize } = require("./config/connect");
-const app = express();
+const helmet = require("helmet");
+const { rateLimit } = require("express-rate-limit");
+
 require("dotenv").config();
 
 const allowedOrigins = [process.env.FRONTEND_URL, process.env.DEV_URL];
@@ -23,8 +26,22 @@ const corsOptions = {
   methods: ["GET", "POST", "PUT", "DELETE", "HEAD", "PATCH"],
 };
 
+//from express-rate-limit with a few changes.
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  limit: 50, // Limit each IP to 50 requests per `window` (here, per 10 minutes).
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
 ///// Middleware /////
 app.use(express.json());
+
+app.use(helmet());
+
+app.use(limiter);
 
 app.use(cors(corsOptions));
 
